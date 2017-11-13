172b1da191df542b3aa98bb02aa1be059be00c13
Jorge <jorgeckw@gmail.com>
2017-11-13 02:29:36 -0200

    Primeira versão do webserver

---
+++ www/cgi-bin/webserver.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import cgi, cgitb
+import socket
+
+cgitb.enable()
+print("Content-Type: text/html;charset=utf-8\r\n\r\n")
+
+#Instanciando o FieldStorage
+form = cgi.FieldStorage()
+vetorComandos =[]
+
+#Pegando os valores dos campos para cada máquina
+for i in range(1, 3):
+    if form.getvalue('maq' + str(i) + '_ps'):
+        comando = "ps"
+        if form.getvalue('maq' + str(i) + '-ps'):
+            parametro = form.getvalue('maq' + str(i) + '-ps')
+            comando = comando + " " + parametro
+        vetorComandos.append(comando)
+
+    if form.getvalue('maq' + str(i) + '_df'):
+        comando = "df"
+        if form.getvalue('maq' + str(i) + '-df'):
+            parametro = form.getvalue('maq' + str(i) + '-df')
+            comando = comando + " " + parametro
+        vetorComandos.append(comando)
+
+    if form.getvalue('maq' + str(i) + '_finger'):
+        comando = "finger"
+        if form.getvalue('maq' + str(i) + '-finger'):
+            parametro = form.getvalue('maq' + str(i) + '-finger')
+            comando = comando + " " + parametro
+        vetorComandos.append(comando)
+
+    if form.getvalue('maq' + str(i) + '_uptime'):
+        comando = "uptime"
+        if form.getvalue('maq' + str(i) + '-uptime'):
+            parametro = form.getvalue('maq' + str(i) + '-uptime')
+            comando = comando + " " + parametro
+        vetorComandos.append(comando)
+
+for i in range(len(vetorComandos)):
+    print(vetorComandos[i] + "<br><br>")
